8e53dffc1415c9f6b29c486864ad67eeb7709d4b
Stevemagegod <Stevemagegod@yahoo.com>
2013-03-24 17:53:00 -0300

    Update GFGoblins.java

--- src/Powerbot/GFGoblins.java
+++ src/Powerbot/GFGoblins.java
@@ -5,22 +5,22 @@ import org.powerbot.game.api.methods.interactive.NPCs;
 import org.powerbot.game.api.methods.interactive.Players;
 import org.powerbot.game.api.wrappers.Tile;
 
-@Manifest(authors = ("Graser"), name = "Good Fight Goblins", description = "Kills Goblins", version = 1)
+@Manifest(authors = ("Graser"), name = "Good Fight Goblins", description = "Kills Goblins", version = 2)
 public class GFGoblins extends ActiveScript {
 
-  /**
-	 * @param args
+	/**
+	 * @param args Untested
 	 */
 	
 	boolean weareinArea = false;
 	boolean wearedead = false;
-	int Goblins = 0;
-	Tile[] DeathWalk = null;
+	int Goblins = 0; //TODO
+	Tile[] DeathWalk = null; //TODO
 
 	@Override
 	public int loop() {
-		if(weareinArea)
-			NPCs.getNearest(Goblins);//Gets The nearest Goblin with one of these Ids if any present, else null.
+		if(weareinArea) //if we are in the Goblin area
+			NPCs.getNearest(Goblins);//we should get The nearest Goblin with one of these Ids if any present, else null.
 		if(NPCs.getNearest(Goblins).isOnScreen()) //Determines if this entity is on screen.
 			NPCs.getNearest(Goblins).click(true); //Clicks this entity.
 		if(NPCs.getNearest(Goblins).isInCombat()) //if the Goblin is in Combat
@@ -30,12 +30,13 @@ public class GFGoblins extends ActiveScript {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			NPCs.getNearest(Goblins).validate();// Verify's that the Goblins exists
-		while(Players.getLocal().isInCombat()); //while we are in combat
-		Players.getLocal().getHealthPercent(); //we should be checking are health
-				if(Players.getLocal().getHealthPercent() &lt;= 60); //if are health is less than 60 we need to eat
-			Eating(); //We should be Eating
-		if(Players.getLocal().isIdle() &amp;&amp; !Players.getLocal().isInCombat()) //If we are idle and we are not in Combat
+		do
+			if(NPCs.getNearest(Goblins).getInteracting() != null); //checks if goblins are interacting
+		while(Players.getLocal().isInCombat());  //while we are in combat
+		Players.getLocal().getHealthPercent(); //we should be checking are health and 
+				if(Players.getLocal().getHealthPercent() <= 60) //if are health is less than 60 we need to eat
+			Eating(); //We should be Eating now
+		if(Players.getLocal().isIdle() && !Players.getLocal().isInCombat()) //If we are idle and we are not in Combat
 			Looting(); //We should be Looting
 		else
 			if(wearedead) //if we are dead
