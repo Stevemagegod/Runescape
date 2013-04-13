a8c25b22f57768753584680c7d8413840bc4d7a6
Stevemagegod <Stevemagegod@yahoo.com>
2013-04-13 15:35:21 -0300

    Update Graserstasks.java

--- src/GrasersTasks/Graserstasks.java
+++ src/GrasersTasks/Graserstasks.java
@@ -8,6 +8,84 @@ package GrasersTasks;
  *
  * @author Stephen
  */
-public class Graserstasks {
-    
-}
+
+    /**Lumbridge Easy Tasks Algorithim
+	 * 
+	 * Start at Lumbridge Lodestone.
+	 * Walk to Lumbridge Cows.
+	 * If Gate To Lumbridge Cows is Closed Open it.
+	 * Kill Cow and Take Cowhide.
+	 * 
+	 * Walk to Al Kahrid
+	 * Open Al Kahrid Gate
+	 * Continue walking to Tanning Store
+	 * If at Tanning Store Trade Ellis
+	 * Tan 1 Soft Leather
+	 * Walk North East and Buy a "Needle, and Thread" from Dommik's Crafting Store
+	 * Use Needle on Thread to make Leather Gloves
+	 * 
+	 * Walk to Al Kahid Mine
+	 * Mine Iron Rocks
+	 * 
+	 * Walk Back to Al Kahrid Gate
+	 * Open Gate
+	 * Walk to Goblin Fishing Spot
+	 * Bait Fishing Spot
+	 * 
+	 * Walk to Lumbridge Furnace
+	 * If At Lumbridge Furnace
+	 * Use Iron Ore on Furnace
+	 * 
+	 * Walk to Zanaris Shed
+	 * If at Zanaris Location
+	 * Open Shed
+	 * If we Got Teleported to other Dimension Log out.
+	 * If we didn't Open Door and Kill a Rat
+	 * Take Raw Rat Meet
+	 * Find Dead Tree 
+	 * If found Chop it down
+	 * 
+	 * Use Tinderbox on Logs
+	 * Use Raw Rat Meat on Fire
+	 * Walk to Water Alter
+	 * If At Water Alter Enter it
+	 * Use Essence on Alter to Craft Water Rune
+	 * 
+	 * Walk to Priest House
+	 * If at House, open door
+	 * Talk to Father Urhney
+	 * Choose Option 3
+	 * 
+	 * Lumbridge Home Teleport Dynar
+	 * Walk to DJail
+	 * If at Jail open TrapDoor
+	 * Climb-Down Trapdoor
+	 * If Task Compleat Climb Back up
+	 * 
+	 * Walk to Wise Old Man
+	 * If Wise Old Man Door Closed Open it
+	 * Talk To Wise Old Man
+	 * Choose Option 2
+	 * 
+	 * Climb up Wise Old Man Stair Case;
+	 * Observe Telescope
+	 * WaitforCutawayScene
+	 * 
+	 * OpenBank
+	 * if(Bankisopen());
+	 * BankClose
+	 * 
+	 * Walk to Wizards Tower
+	 * if at wizards tower open door if closed
+	 * Climbupstaircase
+	 * if at top floor
+	 * open door if closed
+	 * if Demon is on screen
+	 * Tount Through Railing
+	 * Climb Down Stair Case till Basement
+	 * if at Basement
+	 * OpenSedridorsDoor
+	 * if Sedridor is on screen
+	 * right click Teleport
+	 * 
+	 */
